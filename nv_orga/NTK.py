2b8567947724e5abd200438d7a5358f562f8fd51
samsongourevitch <samson.gourevitch@gmail.com>
2023-11-21 14:41:24 +0100

    New orga

--- nv_orga/NTK.py
+++ nv_orga/NTK.py
@@ -3,33 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 
-
-
-class Net_eNTK(nn.Module):
-    def __init__(self):
-        super(Net_eNTK, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
-        self.fc1 = nn.Linear(2048, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
-
-
 def compute_eNTK(model, X, subsample_size=100000, seed=123):
-    """"compute eNTK"""
+    """"compute eNTK
+    
+    prend un modèle de la classe Net_eNTK et un tenseur X en entrée et renvoie une matrice de taille (X.size()[0], subsample_size) contenant les vecteurs de gradient subsamplés pour chaque échantillon de X.
+    """
     model.eval()
     params = list(model.parameters()) #liste de tous les paramètres trainable du modèle
     torch.manual_seed(seed)
